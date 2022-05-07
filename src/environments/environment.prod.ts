61fd4da41971d0798103162caf4e72a8f813c6f6
Denis <wolf-den@tut.by>
2022-05-07 22:14:26 +0300

    fix: prod api cors port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://deploy-preview-5--pomakom-pm.netlify.app/api',
+  apiURL: 'https://deploy-preview-5--pomakom-pm.netlify.app/api:443',
 };
