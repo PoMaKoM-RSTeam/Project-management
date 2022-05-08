c75580bd22eb4462d476b91d31d4cb2af8bb7f85
Denis <wolf-den@tut.by>
2022-05-08 14:58:12 +0300

    feat: get all boards

--- src/app/space/components/left-aside/left-aside.component.ts
+++ src/app/space/components/left-aside/left-aside.component.ts
@@ -1,18 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { LinkIcon } from '../../../../constants/enums';
+import { BoardsAPIService } from '../../../../services/boards-api.service';
+import { Board } from '../../../../models/column.model';
 
 @Component({
   selector: 'space-left-aside',
   templateUrl: './left-aside.component.html',
   styleUrls: ['./left-aside.component.scss'],
 })
-export class LeftAsideComponent {
+export class LeftAsideComponent implements OnInit {
+  constructor(private boardsAPIService: BoardsAPIService) {}
+
   @Input() pageName: string = 'Page Name';
 
   @Input() title: string = 'Title';
 
+  iconBoard = LinkIcon.BOARD;
+
   isCollapsed = false;
 
   isSpaceSearchVisible = false;
+
+  boards: Board[] = [];
+
+  ngOnInit() {
+    this.boardsAPIService.getAllBoards().subscribe((response) => {
+      this.boards = response;
+    });
+  }
 
   stopPropagation(event: Event) {
     event.stopPropagation();
