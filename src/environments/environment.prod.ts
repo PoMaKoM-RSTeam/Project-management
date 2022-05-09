340f0592b58767360f10716cc8f185ce3b9cd216
Denis <wolf-den@tut.by>
2022-05-09 14:41:05 +0300

    cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 
 export const environment = {
   production: true,
-  apiURL: 'https://nest-backend-den.herokuapp.com/api',
+  apiURL: 'https://nest-backend-den.herokuapp.com',
   auth_token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4MWUxMGExNC1mN2Q3LTQ0MTYtOGJmOS1hYjVmZmUzZjMyMTIiLCJsb2dpbiI6InVzZXIwMDEiLCJpYXQiOjE2NTE0MjgxMDZ9.ZmSzIt14wNdi_-pxgrn88d9peWn3BJHEK2CkmS3snVQ`,
 };
