46eb7beeeb2cfaaa9a5413de2cc6bb89e5e405ae
Oleksandr <algolj@algolj.it>
2022-05-11 23:52:53 +0300

    fix: eslint errors

--- src/app/core/pipes/search/search.pipe.spec.ts
+++ src/app/core/pipes/search/search.pipe.spec.ts
@@ -3,6 +3,7 @@ import { SearchPipe } from './search.pipe';
 describe('SearchPipe', () => {
   it('create an instance', () => {
     const pipe = new SearchPipe();
+
     expect(pipe).toBeTruthy();
   });
 });
