703dbbcc5555caf7ee02f10410dcad6829d0c6ca
Denis <wolf-den@tut.by>
2022-05-15 19:21:22 +0300

    fix: remove console log

--- src/app/space/components/space-nav/space-nav.component.ts
+++ src/app/space/components/space-nav/space-nav.component.ts
@@ -22,7 +22,6 @@ export class SpaceNavComponent implements OnInit {
   }
 
   changeActive(title: string | undefined) {
-    console.log('title', title, this.links);
     if (title) {
       this.spaceHeaderService.setActive(title);
     }
